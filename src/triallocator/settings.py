f4adbc4e9f88eb2ecbaa96946d353299b2d60350
Jeff Rogers <jeff@triallocator.org>
2024-02-29 20:33:30 -0800

    Add custom user model

--- src/triallocator/settings.py
+++ src/triallocator/settings.py
@@ -36,14 +36,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'home.apps.HomeConfig',
-    'trials.apps.TrialsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'home',
+    'trials',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "accounts.User"
