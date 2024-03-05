5cae2dabb2403013f5fd7addbadb1672f7c0125c
Jeff Rogers <jeff@triallocator.org>
2024-03-05 08:06:06 -0800

    Add about page and improve templates

--- src/home/urls.py
+++ src/home/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
   path("donate/", views.donate, name="donate"),
   path("volunteer/", views.volunteer, name="volunteer"),
   path("privacy/", views.privacy, name="privacy"),
+  path("questions-and-answers/",
+       views.questions_and_answers,
+       name="questions-and-answers"),
+  path("contact/", views.contact, name="contact"),
+  path("financials/", views.financials, name="financials"),
 ]
--- src/home/views.py
+++ src/home/views.py
@@ -8,13 +8,35 @@ def index(request):
   return HttpResponse(template.render(context, request))
 
 def about(request):
-  return HttpResponse("Hello, world. You're at the triallocator.org about page.")
+  template = loader.get_template("home/about.html")
+  context = {}
+  return HttpResponse(template.render(context, request))
 
 def donate(request):
-  return HttpResponse("Hello, world. You're at the triallocator.org donation page.")
+  template = loader.get_template("home/donate.html")
+  context = {}
+  return HttpResponse(template.render(context, request))
 
 def volunteer(request):
-  return HttpResponse("Hello, world. You're at the triallocator.org volunteering page.")
+  template = loader.get_template("home/volunteer.html")
+  context = {}
+  return HttpResponse(template.render(context, request))
 
 def privacy(request):
   return HttpResponse("Hello, world. You're at the triallocator.org privacy page.")
+
+def questions_and_answers(request):
+  template = loader.get_template("home/q-and-a.html")
+  context = {}
+  return HttpResponse(template.render(context, request))
+
+def contact(request):
+  template = loader.get_template("home/contact.html")
+  context = {}
+  return HttpResponse(template.render(context, request))
+
+def financials(request):
+  template = loader.get_template("home/financials.html")
+  context = {}
+  return HttpResponse(template.render(context, request))
+
