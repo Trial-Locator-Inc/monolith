6984c37f2b8ccd18f55db18d7e6565a979976954
Jeff Rogers <jeff@triallocator.org>
2024-03-05 23:06:12 -0800

    Add financials page and add active tab selection to other pages

--- src/home/views.py
+++ src/home/views.py
@@ -4,31 +4,31 @@ from django.template import loader
 
 def index(request):
   template = loader.get_template("home/index.html")
-  context = {}
+  context = { "home_page_active": True, }
   return HttpResponse(template.render(context, request))
 
 def about(request):
   template = loader.get_template("home/about.html")
-  context = {}
+  context = { "about_page_active": True, }
   return HttpResponse(template.render(context, request))
 
 def donate(request):
   template = loader.get_template("home/donate.html")
-  context = {}
+  context = { "donate_page_active": True, }
   return HttpResponse(template.render(context, request))
 
 def volunteer(request):
   template = loader.get_template("home/volunteer.html")
-  context = {}
+  context = { "volunteer_page_active": True, }
+  return HttpResponse(template.render(context, request))
+
+def questions_and_answers(request):
+  template = loader.get_template("home/q-and-a.html")
+  context = { "q_and_a_page_active": True, }
   return HttpResponse(template.render(context, request))
 
 def privacy(request):
   return HttpResponse("Hello, world. You're at the triallocator.org privacy page.")
-
-def questions_and_answers(request):
-  template = loader.get_template("home/q-and-a.html")
-  context = {}
-  return HttpResponse(template.render(context, request))
 
 def contact(request):
   template = loader.get_template("home/contact.html")
