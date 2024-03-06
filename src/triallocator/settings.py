9061a6ff991c24710d16856efbc4cfc2b3feffce
Jeff Rogers <jeff@triallocator.org>
2024-03-05 23:35:49 -0800

    Install alpinejs and htmx

--- src/triallocator/settings.py
+++ src/triallocator/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'django_browser_reload',
+    'django_htmx',
 
     'home',
     'trials',
@@ -63,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_browser_reload.middleware.BrowserReloadMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'triallocator.urls'
